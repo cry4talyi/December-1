f688589d9b84ceed2580e24c4973e67810133e69
郭兆龙 <1094855085@qq.com>
2018-12-18 14:15:11 +0800

    更新了跳转博客以及跳转讨论组的功能

--- spring-security-step-by-step/src/main/java/cn/com/taiji/controller/HomeController.java
+++ spring-security-step-by-step/src/main/java/cn/com/taiji/controller/HomeController.java
@@ -4,11 +4,15 @@ import cn.com.taiji.domain.Blog;
 import cn.com.taiji.domain.ChatTeam;
 import cn.com.taiji.repository.BlogRepository;
 import cn.com.taiji.repository.ChatTeamRepository;
+
+import cn.com.taiji.service.impl.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +28,9 @@ import java.util.Optional;
  */
 @Controller
 public class HomeController {
+    @Autowired
+    Service service;
+
     @GetMapping({"","/","/index"})
     public String index(Model model) {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -40,6 +47,29 @@ public class HomeController {
     public String login() {
         return "/login";
     }
+
+    /**
+     * @author 郭兆龙
+     * 跳转到讨论组页面
+     * @param model
+     * @return
+     */
+    @GetMapping("/ct")
+    public String chatTeam(Model model){
+        model.addAttribute("chat",service.chat());
+        return "ct";
+    }
+
+    /**
+     * @Author 郭兆龙
+     * 跳转到博客
+     */
+    @GetMapping("/blog")
+    public String blog(Model model){
+        model.addAttribute("post",service.blog());
+        return "blog";
+    }
+
 
 
 
